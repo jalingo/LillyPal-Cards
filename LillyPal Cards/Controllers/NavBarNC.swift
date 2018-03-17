ae00d362b4a7e7f24c466fd2b5c6661b36632e2f
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-17 00:31:52 -0700

    trimmed

--- LillyPal Cards/Controllers/NavBarNC.swift
+++ LillyPal Cards/Controllers/NavBarNC.swift
@@ -15,8 +15,4 @@ class NavBarNC: UINavigationController {
 
         // Do any additional setup after loading the view.
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-print(" prep @ navBar \(segue.destination.description)")
-    }
 }
