d2b0ef3b7e8eaa83cd5494d0516747c836f9c1d5
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-17 02:27:14 -0700

    moved decoration to viewWillAppear to keep nav bar hidden

--- LillyPal Cards/Controllers/Character Sheet Editor/Protocols/CharacterPortraitChanger.swift
+++ LillyPal Cards/Controllers/Character Sheet Editor/Protocols/CharacterPortraitChanger.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 
+let characterPortraits = [#imageLiteral(resourceName: "cool_kitty_cat"), #imageLiteral(resourceName: "Quick_rick"), #imageLiteral(resourceName: "steve"), #imageLiteral(resourceName: "Wakil_kepala_yavno"), #imageLiteral(resourceName: "bully_boy_bob")]
+
 protocol CharacterPortraitChanger { }
 
 extension CharacterPortraitChanger where Self: CharacterSheetVC {
     
     // !!
     func nextPortrait() -> UIImage {
-        return UIImage()
+        guard let img = self.characterPortrait.image, let current = characterPortraits.index(of: img) else { return #imageLiteral(resourceName: "cool_kitty_cat") }
+
+        if current + 1 < characterPortraits.count - 1 {
+            return characterPortraits[current + 1]
+        } else {
+            return characterPortraits.first ?? #imageLiteral(resourceName: "cool_kitty_cat")
+        }
     }
     
     // !!
     func previousPortrait() -> UIImage {
-        return UIImage()
+        guard let img = self.characterPortrait.image, let current = characterPortraits.index(of: img) else { return #imageLiteral(resourceName: "cool_kitty_cat") }
+
+        if current - 1 > -1 {
+            return characterPortraits[current - 1]
+        } else {
+            return characterPortraits.last ?? #imageLiteral(resourceName: "cool_kitty_cat")
+        }
     }
 }
--- LillyPal Cards/Controllers/Character Sheet List/MainViewController.swift
+++ LillyPal Cards/Controllers/Character Sheet List/MainViewController.swift
@@ -55,9 +55,8 @@ class MainViewController: UIViewController, MainViewDecorator {
     
     // MARK: - Functions: UIViewController
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.decorate()
     }
     
