38cc8915292b8de365edad2b0e20d4fb353b8094
Daria Lingo <koshmanova.91@mail.ru>
2022-06-23 14:58:02 -0500

    LPC-00001 [Daria] changed application sleeping time to 3 sec.

--- LillyPal Cards/AppDelegate.swift
+++ LillyPal Cards/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        do {sleep(3)}
         return true
     }
 
